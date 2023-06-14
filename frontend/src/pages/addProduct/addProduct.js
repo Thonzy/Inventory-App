6d3ff22615f092e500e416295bc4ecd4c20267fe
Thon <manalangfordy@gmail.com>
2023-06-14 15:20:45 +0800

    commit

---
+++ frontend/src/pages/addProduct/addProduct.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
+import ProductForm from "../../components/product/productForm/ProductForm";
+import {
+    createProduct,
+    selectIsLoading,
+} from "../../redux/features/product/productSlice";
